aba3c7d429b0acc971422a8dec0f50408e831bfa
FlashNemo <temeryazevmark@bk.ru>
2024-06-24 21:03:08 +1000

    Add files via upload

---
+++ ПервоеДомЗа.py
@@ -0,0 +1,28 @@
+# первое задание
+a = "FlashNemo"
+print(len(a))
+# второе задание
+first=10
+second = 5
+summa = (first + second)
+diff = (first - second)
+print(summa)
+print(diff)
+# третье задание
+first = 5
+second = 10
+third = 15
+mean= ((5+10+15)/3)
+print(mean)
+# четвертое задание
+first_string = "вторник"
+second_string = "понедельник"
+print(second_string + ", " + first_string)
+# пятое задание
+a = 6
+b = 5
+c = 7
+f = ((a*b)+(a*c))
+print((((a*b)+(a*c))**3)/2)
+
+
